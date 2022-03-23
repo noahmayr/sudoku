ffc14fb01bdef14e1d035cfe7b442fad6bf69555
Noah Mayr <dev@noahmayr.com>
2022-03-23 17:58:06 +0100

    Update styling

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -14,13 +14,13 @@ const Cell = ({ cell }: CellProps) => {
         <svg x={cell.x} y={cell.y} width={1} height={1} className={classes.cell}>
             <rect x="0%" y="0%" width="100%" height="100%" className={classes.cellRect} />
             {
-                (value ===undefined && state.center !== undefined) ? (
+                (value === undefined && state.center !== undefined) ? (
                     <text width="1" height="1" x="50%" y="50%" className={classes.centerMark} data-count={state.center.size}>
                         {Array.from(state.center?.values()).sort()}
                     </text>
                 ) : null
             }
-            <text width="1" height="1" x="50%" y="50%" className={classes.cellValue}>
+            <text width="1" height="1" x="50%" y="50%" className={isGiven ? classes.given : classes.cellValue}>
                 {value}
             </text>
         </svg>
