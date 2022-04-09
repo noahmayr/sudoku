c4abe5cafa11e29bda853bf73a953870537e51a1
Noah Mayr <dev@noahmayr.com>
2022-04-09 10:27:33 +0200

    Fix green validation flash when loading the game

--- src/components/Validation/Validation.tsx
+++ src/components/Validation/Validation.tsx
@@ -33,7 +33,7 @@ const Validation = () => {
         },
     );
 
-    if (dimensions !== undefined && state.filled && state.errors.size === 0) {
+    if (dimensions !== undefined && results.length && state.filled && state.errors.size === 0) {
         return (<rect className={classes.success} {...dimensions} />);
     }
 
